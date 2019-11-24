b51314af15afdd123b0331680b78ac9092ce7e47
litefunc <lite.func@gamil.com>
2019-11-24 22:22:26 +0800

    add git.sh

---
+++ git.sh
@@ -0,0 +1,11 @@
+echo current:
+git config --list
+
+# git config --global user.name "黃奐中"
+# git config --global user.email "david.huang@noovo.co"
+
+git config --global user.name "litefunc"
+git config --global user.email "lite.func@gamil.com"
+
+echo new:
+git config --list
